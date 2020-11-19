db452dabeb73fc60e00da96e17df019da8fd5a91
Maxi Rosson <maxirosson@gmail.com>
2020-11-19 08:13:26 -0300

    Addd dipien-component-builder plugin

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,5 +1,8 @@
 object BuildLibs {
-    // https://github.com/maxirosson/jdroid-gradle-plugin/releases
-    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.1-SNAPSHOT"
-    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.1-SNAPSHOT"
+    // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.5.0-SNAPSHOT"
+    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.5.0-SNAPSHOT"
+
+    // https://github.com/dipien/dipien-component-builder/blob/master/CHANGELOG.md
+    const val DIPIEN_COMPONENT_BUILDER = "com.jdroidtools:dipien-component-builder:3.0.0-SNAPSHOT"
 }
