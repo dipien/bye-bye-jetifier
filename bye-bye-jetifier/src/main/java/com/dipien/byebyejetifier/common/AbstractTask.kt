206da1ee981150afcc84c84715093f4c7152d74a
Maxi Rosson <maxirosson@gmail.com>
2020-11-21 10:05:38 -0300

    Improved project config. Fixed ktlint error

--- bye-bye-jetifier/src/main/java/com/dipien/byebyejetifier/common/AbstractTask.kt
+++ bye-bye-jetifier/src/main/java/com/dipien/byebyejetifier/common/AbstractTask.kt
@@ -4,7 +4,6 @@ import com.dipien.byebyejetifier.ByeByeJetifierGradlePlugin
 import com.dipien.byebyejetifier.ByeByeJetifierExtension
 import org.gradle.api.DefaultTask
 import org.gradle.api.logging.LogLevel
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
