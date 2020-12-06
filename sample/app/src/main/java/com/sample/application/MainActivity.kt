8956113d300c123a0f6f7ee9585d0d9a92d58a90
Maxi Rosson <maxirosson@gmail.com>
2020-12-06 16:27:53 -0300

    Sample app improvements

---
+++ sample/app/src/main/java/com/sample/application/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sample.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ sample/buildSrc/src/main/kotlin/BuildLibs.kt
@@ -0,0 +1,6 @@
+object BuildLibs {
+
+    const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:4.1.1"
+    const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20"
+    const val BYE_BYE_JETIFIER_GRADLE_PLUGIN = "com.dipien:bye-bye-jetifier:1.0.0-SNAPSHOT"
+}
