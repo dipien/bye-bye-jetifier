72afa968ac9dace98b1cc1b51d395dd6671d9418
maxirosson <maxirosson@gmail.com>
2021-02-12 22:20:08 -0300

    Revert some changes

--- bye-bye-jetifier/src/main/java/com/dipien/byebyejetifier/ProjectAnalyzer.kt
+++ bye-bye-jetifier/src/main/java/com/dipien/byebyejetifier/ProjectAnalyzer.kt
@@ -2,7 +2,6 @@ package com.dipien.byebyejetifier
 
 import com.dipien.byebyejetifier.archive.Archive
 import com.dipien.byebyejetifier.common.LoggerHelper
-import com.dipien.byebyejetifier.common.toFilePath
 import com.dipien.byebyejetifier.scanner.ScanResult
 import com.dipien.byebyejetifier.scanner.ScannerProcessor
 import org.gradle.api.Project
@@ -19,11 +18,6 @@ class ProjectAnalyzer(
     private val scannerProcessor: ScannerProcessor,
     private val excludeSupportAnnotations: Boolean
 ) {
-
-    companion object {
-        val ANDROID_SUPPORT_ANNOTATION_PATH = "android/support/annotation".toFilePath()
-        const val ANDROID_SUPPORT_ANNOTATION_PACKAGE = "android.support.annotation"
-    }
 
     fun analyze(projectAnalyzerResult: ProjectAnalyzerResult) {
 
@@ -146,8 +140,8 @@ class ProjectAnalyzer(
     private fun filterSupportAnnotationsIfNeeded(results: List<ScanResult>): List<ScanResult> {
         if (excludeSupportAnnotations) {
             return results.filter { scanResult ->
-                !scanResult.legacyDependency.startsWith(ANDROID_SUPPORT_ANNOTATION_PATH) &&
-                    !scanResult.legacyDependency.startsWith(ANDROID_SUPPORT_ANNOTATION_PACKAGE)
+                !scanResult.legacyDependency.startsWith("android/support/annotation") &&
+                    !scanResult.legacyDependency.startsWith("android.support.annotation")
             }
         }
         return results
