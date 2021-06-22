421001114f3fa0fd6521c60178f5370a807ef084
Dipien CI <74492091+dipien-ci@users.noreply.github.com>
2021-06-22 13:23:13 -0300

    Upgraded dependencies for groupId org.ow2.asm (#34)
    * Upgraded org.ow2.asm:asm from 9.0 to 9.1
    
    * Upgraded org.ow2.asm:asm-util from 9.0 to 9.1
    
    * Upgraded org.ow2.asm:asm-commons from 9.0 to 9.1

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,9 +1,9 @@
 object Libs {
 
     // https://asm.ow2.io/versions.html
-    const val ASM = "org.ow2.asm:asm:9.0"
-    const val ASM_UTILS = "org.ow2.asm:asm-util:9.0"
-    const val ASM_COMMONS = "org.ow2.asm:asm-commons:9.0"
+    const val ASM = "org.ow2.asm:asm:9.1"
+    const val ASM_UTILS = "org.ow2.asm:asm-util:9.1"
+    const val ASM_COMMONS = "org.ow2.asm:asm-commons:9.1"
 
     // https://github.com/junit-team/junit4/releases
     const val JUNIT = "junit:junit:4.13.2"
