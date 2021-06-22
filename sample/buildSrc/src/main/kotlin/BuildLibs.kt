fca48500ad7575a464cabdf53ef6c34a27fe065e
Maxi Rosson <maxirosson@gmail.com>
2021-06-22 09:55:36 -0300

    Upgrade to version 1.2.0

--- sample/buildSrc/src/main/kotlin/BuildLibs.kt
+++ sample/buildSrc/src/main/kotlin/BuildLibs.kt
@@ -2,5 +2,5 @@ object BuildLibs {
 
     const val ANDROID_GRADLE_PLUGIN = "com.android.tools.build:gradle:4.1.1"
     const val KOTLIN_GRADLE_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.20"
-    const val BYE_BYE_JETIFIER_GRADLE_PLUGIN = "com.dipien:bye-bye-jetifier:1.1.2"
+    const val BYE_BYE_JETIFIER_GRADLE_PLUGIN = "com.dipien:bye-bye-jetifier:1.2.0-SNAPSHOT"
 }
