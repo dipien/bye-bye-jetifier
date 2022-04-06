6f36c16bda5a9dfcf1385bd3a7999b7320dcc6d0
dipien-ci <dipien2020@gmail.com>
2022-04-06 13:35:35 +0000

    Upgraded org.ow2.asm:asm from 9.2 to 9.3

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,7 +1,7 @@
 object Libs {
 
     // https://asm.ow2.io/versions.html
-    const val ASM = "org.ow2.asm:asm:9.2"
+    const val ASM = "org.ow2.asm:asm:9.3"
     const val ASM_UTILS = "org.ow2.asm:asm-util:9.2"
     const val ASM_COMMONS = "org.ow2.asm:asm-commons:9.2"
 
